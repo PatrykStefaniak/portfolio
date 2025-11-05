c44ce787058357e6a4c4c5db00fe1bc5009f1ed7
Bombo <patryk080998@gmail.com>
2025-11-05 09:39:32 +0900

    Removed scroll and added a progress bar instead

--- components/Home.tsx
+++ components/Home.tsx
@@ -6,9 +6,10 @@ import Separator from "./Separator";
 export default function Main() {
     return <div id="container" className="overflow-clip">
         <div className="bg-radial-[at_5%_5%] to-(--bg) from-(--bg-light) fixed inset-0 ">
-            <Background />
+            <Background/>
         </div>
         <main className="z-10 relative">
+            <div className="animate-progress fixed h-0 border-2 border-(--border) top-0 right-0"/>
             <Welcome/>
             <Separator/>
             <ProjectList/>
--- components/ui/LinkButton.tsx
+++ components/ui/LinkButton.tsx
@@ -12,6 +12,7 @@ export default function LinkButton(props: LinkButtonProps) {
     return (
         <div>
             <a
+                target="_blank"
                 href={url}
                 className="flex gap-2 border-2 p-[7] rounded-lg border-(--border) hover:bg-(--bg-lighter) hover:border-(--text-dark)"
             >
